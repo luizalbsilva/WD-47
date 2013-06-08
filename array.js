4b70e12d913cb2269d01128630aab5c41557409f
Luiz Alberoni da Silva <luizalbsilva@gmail.com>
2013-06-08 11:56:39 -0300

    trabalhando com arrays

---
+++ array.js
@@ -0,0 +1,20 @@
+// sintaxe literal
+var frutas = ["morango", "banana", "maçã", "laranja"]
+
+// sintaxe construtor
+var fruits = new Array("Strawberry", "banana", "apple", "orange")
+
+fruits.push("Kiwi")
+console.log(frutas, typeof(frutas), frutas.construtor)
+console.log(fruits, typeof(fruits), fruits.construtor)
+
+
+var 
+	nome = "Luiz", 
+	name = new String("Luiz")
+
+console.log(nome, typeof(nome), nome.construtor)
+console.log(name, typeof(name), name.construtor)
+
+console.dir(nome)
+console.dir(name)
