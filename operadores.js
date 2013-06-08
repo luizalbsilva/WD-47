d079a6c34c61ad789c14c17644b323363f446828
Luiz Alberoni da Silva <luizalbsilva@gmail.com>
2013-06-08 11:24:31 -0300

    Demonstrando operadores diversos

---
+++ operadores.js
@@ -0,0 +1,38 @@
+// Incremento / decremento
+var x =1
+console.log(x=x+1) // 2
+console.log(x++)   // 2  com x = 3
+console.log(x)
+console.log(x=x-1) // 2
+console.log(x--)   // 2 com x = 1
+console.log(x)
+console.log(x+=4)
+
+// Matemáticos
+console.log("adição", 1+1)
+console.log("subtração", 2-1)
+console.log("multiplicação", 2*2)
+console.log("divisão", 4/2)
+console.log("Resto", 5%2)
+
+// Comparação
+console.log("maior que",5>2)
+console.log("menor que",3<5)
+console.log("maior ou igual",5>=5)
+console.log("menor ou igual",4<=5)
+console.log("igual a(==)",2=="2")
+console.log("igual a(===)",2==="2")
+console.log("diferente de(!=)",3!="3")
+console.log("diferente de(!==)",3!=="3")
+
+// Operadores Lógicos
+console.log("AND",1==1 && 2==2)
+console.log("OR",1==1 || 2==1)
+
+// Ternário
+console.log("Ternário",1==2?"um":"Diferente de um")
+
+// Bitwise
+console.log("or", 2 | 1)
+console.log("and", 3 & 2)
+console.log("xand", 3 ^ 2)
