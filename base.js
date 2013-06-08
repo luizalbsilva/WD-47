b7bb4634107e359ce8baad29090b27c8c42a9950
Luiz Alberoni da Silva <luizalbsilva@gmail.com>
2013-06-08 10:34:51 -0300

    Visualizando variáveis através de console.log

--- base.js
+++ base.js
@@ -1,1 +1,8 @@
-alert("Hello world")
+// alert("Hello world")
+var escola = "Caelum",
+	curso = "Javascript",
+	instrutor = "Leonardo",
+	cargaHoraria = "20hrs"
+
+
+console.log(escola, curso, instrutor, cargaHoraria)
